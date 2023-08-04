b5c94dd266217ec9e2cfda3e643311f6b3351cc8
Sarang Landge <saranglandge28@gmail.com>
2023-08-04 14:07:17 +0530

    finished project

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,40 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which service is used for sending or receiving mail on the internet?",
+           " Which planet have the ‘Great Red Spot’?",
+            "Which one is the smallest ocean in the World?",
+            "Which country gifted the ‘Statue of Liberty’ to USA in 1886?",
+            "Which country is known as the ‘playground of Europe’?",
+            "Total number of oceans in the World is",
+            " Which country is also known as the ‘Land of Thousand Lakes"
+
+    };
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Email","internet","skype","twitter"},
+            {"Jupiter","Mars","Neptune","Moon"},
+            {"Indian","pacific","Atlantic","Arctic"},
+            {"France","Canada","Brazil","England"},
+            {"Austria","Holland","Switzerland","Italy"},
+            {"1","5","12","7"},
+            {"Norway","Finland","Iceland","Switzerland"}
+    };
+    public static String correctAnswer[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Email",
+            "Jupiter",
+            "Arctic",
+            "France",
+            "Switzerland",
+            "5",
+            "Finland"
+
+    };
+}
